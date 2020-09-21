47c3039d17c0558e1c8d8e52e09dbe451d211ccd
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-21 01:55:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |ray|
     puts "#{ray.capitalize}!"
   end
-  planeteer_calls
 end
 
 def long_planeteer_calls(planeteer_calls)
