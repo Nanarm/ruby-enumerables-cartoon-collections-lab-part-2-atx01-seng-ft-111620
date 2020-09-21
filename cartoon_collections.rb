4173b192ca6eae4de579050c2b0e42892af06f9a
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-21 02:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |calls|
     call == "Earth!" || "Wind!" || "Fire!" || "Water!" || "Heart!"
+  end
 end
 
 
